d9d0f2229c4b30a761cd85538a6ea04c547a74cd
medygoo <lomsmedy@gmail.com>
2026-08-14 11:41:57 +0100

    Préparer le raccordement SchoolSafe sans lien actif

---
+++ assets/schoolsafe-config.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  /*
+   * Le site reste autonome tant que l'instance SchoolSafe de l'ecole
+   * n'est pas reellement disponible. Lors du raccordement, renseigner ici
+   * son adresse HTTPS officielle, par exemple https://app.cslesage.com/.
+   */
+  window.SCHOOLSAFE_SITE_CONFIG = Object.freeze({
+    appUrl: ''
+  });
+})();
